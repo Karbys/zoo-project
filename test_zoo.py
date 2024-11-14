bafb3cdbf5d9b814c455969d553f0c3f377af60a
Karbys <98176887+Karbys@users.noreply.github.com>
2024-11-14 07:21:58 +0000

    Add new test

--- test_zoo.py
+++ test_zoo.py
@@ -5,9 +5,18 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_child_ticket_price(self):
+    def test_child_ticket_price_C1b1(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50) # Fault fixed
+    def test_child_ticket_price_C1b2(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100) # Fault fixed
+    def test_child_ticket_price_C1b3(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "undefined") # Fault fixed
+    def test_child_ticket_price_C1b4(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150) # Fault fixed
+    def test_child_ticket_price_C1b5(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
+    
     # Add your additional test cases here.
 
 if __name__ == '__main__':
